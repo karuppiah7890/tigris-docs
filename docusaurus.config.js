9876c1b5152b610ea0051d86d1fa4e16f2ad2240
Adil Ansari <ansari.adil20@gmail.com>
2022-08-26 01:28:31 -0700

    Api sidebar (#123)
    * feat: Added observability API docs
    
    * feat: http api sidebar
    
    * refactor: Moved docs to top level
    
    Co-authored-by: Jigar Joshi <jigarjm@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,6 +9,45 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const TwitterSvg =
   '<svg style="fill: #1DA1F2; vertical-align: middle;" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>';
 
+const openApiDocs = [
+  "redocusaurus",
+  {
+    // Plugin Options for loading OpenAPI files
+    specs: [
+      {
+        id: "server-v1-api-openapi",
+        spec: "external/tigris-api/server/v1/api_openapi.yaml",
+      },
+      {
+        id: "server-v1-admin-openapi",
+        spec: "external/tigris-api/server/v1/admin_openapi.yaml",
+      },
+      {
+        id: "server-v1-auth-openapi",
+        spec: "external/tigris-api/server/v1/auth_openapi.yaml",
+      },
+      {
+        id: "server-v1-health-openapi",
+        spec: "external/tigris-api/server/v1/health_openapi.yaml",
+      },
+      {
+        id: "server-v1-observability-openapi",
+        spec: "external/tigris-api/server/v1/observability_openapi.yaml",
+      },
+    ],
+    // Theme Options for modifying how redoc renders them
+    theme: {
+      // Change with your site colors
+      primaryColor: "#38ac8c",
+      theme: {
+        sidebar: {
+          width: "240px",
+        },
+      },
+    },
+  },
+];
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Tigris",
@@ -23,6 +62,7 @@ const config = {
   onBrokenMarkdownLinks: "warn",
 
   presets: [
+    openApiDocs,
     [
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
@@ -57,23 +97,6 @@ const config = {
         },
       },
     ],
-    [
-      "redocusaurus",
-      {
-        // Plugin Options for loading OpenAPI files
-        specs: [
-          {
-            spec: "external/tigris-api/server/v1/api_openapi.yaml",
-            route: "/apidocs/",
-          },
-        ],
-        // Theme Options for modifying how redoc renders them
-        theme: {
-          // Change with your site colors
-          primaryColor: "#38ac8c",
-        },
-      },
-    ],
   ],
 
   plugins: [
@@ -81,6 +104,15 @@ const config = {
       "docusaurus-plugin-segment",
       {
         apiKey: "5hxcYrckK6SUpv5MaebDNAUZ0vJvzFcM",
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "httpdocs",
+        path: "httpdocs",
+        routeBasePath: "httpdocs",
+        sidebarPath: require.resolve("./httpdocs/sidebars.js"),
       },
     ],
   ],
@@ -118,8 +150,9 @@ const config = {
             position: "left",
             items: [
               {
-                label: "HTTP API",
-                to: "apidocs/",
+                label: "HTTP",
+                to: "httpdocs/server/v1/api",
+                activeBasePath: "httpdocs",
               },
               {
                 label: "RPC",
--- generatedSidebarsProtodocs.js
+++ generatedSidebarsProtodocs.js
@@ -7,7 +7,13 @@ module.exports = {
         {
           type: "category",
           label: "/server/v1",
-          items: [{ type: "doc", id: "server/v1/api.proto" }],
+          items: [
+            { type: "doc", id: "server/v1/api.proto" },
+            { type: "doc", id: "server/v1/admin.proto" },
+            { type: "doc", id: "server/v1/health.proto" },
+            { type: "doc", id: "server/v1/auth.proto" },
+            { type: "doc", id: "server/v1/observability.proto" },
+          ],
         },
       ],
     },
