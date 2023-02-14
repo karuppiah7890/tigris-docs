310f3247a0a1f9791897c575355cd676817c8b97
Phil Leggetter <phil@leggetter.co.uk>
2023-02-14 10:31:44 +0000

    chore: fix link to cursor

--- docs/sdkstools/typescript/commons.js
+++ docs/sdkstools/typescript/commons.js
@@ -1,5 +1,5 @@
 export const cursorOps = "findMany()";
-export const cursorDocLink = "/typescript/database/cursor";
+export const cursorDocLink = "/docs/sdkstools/typescript/database/cursor";
 
 // decorator and interface based datamodeling constants
 export const dataModelTabGroupId = "datamodel-tabs";
